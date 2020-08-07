51fe9b2d2dae49c65785174169dfd6c89f3af51f
Rajinkumardevops <rajinkumardevops@gmail.com>
2020-08-06 22:31:14 -0700

    adding inputargiments

--- 04-input-read.sh
+++ 04-input-read.sh
@@ -1,5 +1,5 @@
 #!bib/bash
 
-name=Rajinkumar
+
 read -p 'Enter your name: ' name
 echo "Your name = $name"
---
+++ 05-input-argiments.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+echo script name = $0
+echo first script = $1
+echo second script = $2
